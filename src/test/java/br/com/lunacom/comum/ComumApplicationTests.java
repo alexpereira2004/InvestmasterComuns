1435ac9c10a5adbbd0c0d79eb16cf04f4a0bb69b
alexpereira2014 <alexpereira2004@gmail.com>
2026-06-05 22:12:32 -0300

    #67 Atualiza dependencias

--- src/test/java/br/com/lunacom/comum/ComumApplicationTests.java
+++ src/test/java/br/com/lunacom/comum/ComumApplicationTests.java
@@ -1,13 +1,12 @@
 package br.com.lunacom.comum;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ComumApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
